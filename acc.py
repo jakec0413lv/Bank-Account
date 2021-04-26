42cd320285b478a214b9937ee5a80314baa765a5
Jacob Cox <62845242+jakec0413lv@users.noreply.github.com>
2021-04-25 22:17:36 -0700

    Add files via upload
    Added inheritance example

--- acc.py
+++ acc.py
@@ -21,10 +21,22 @@ class Account:
         with open(self._filepath, 'w') as file:
             file.write(str(self.balance))
 
+class Checking(Account): #Inheritance
 
-account = Account("balance.txt")
+    def __init__(self, filepath, _fee):
+        Account.__init__(self, filepath)
+        self.fee = _fee
 
-account.withdraw(25)
-account.deposit(55)
-account.withdraw(25)
-account.commit()
+    def transfer(self, amount):
+        if self.balance - (amount + self.fee) >= 0:
+            self.balance -= (amount + self.fee)
+            print("New Balance: $" + str(self.balance) + "!")
+        else:
+            print("Insufficient funds!")
+            print("Current Balance: $" + str(self.balance))
+
+
+checking = Checking("balance.txt", 5)
+checking.deposit(150)
+checking.transfer(110)
+checking.commit()
